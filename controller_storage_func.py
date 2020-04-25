a38717bf62e25a95211b955ea245833006c65fe2
Blossom Ghuntla <bghuntla123@gmail.com>
2020-04-25 18:13:47 +0530

    Commit_5: controller storage ui func updated controller storage function of disabling button and enabling, saving the curve command in the json, preserve button function wip, caveats of all the function's refactored, all the pycharm errors rectified

--- controller_storage_func.py
+++ controller_storage_func.py
@@ -6,55 +6,90 @@ from shiboken2 import wrapInstance
 import maya.cmds as cmds
 import controller_storage_ui
 import os
+import json
 
 
 class ControllerStorageFunc(controller_storage_ui.ControllerStorageUI):
 	"""This Class is Func for UI to preserve nurbs curve
 
 	"""
+
 	def __init__(self):
 		super(ControllerStorageFunc, self).__init__()
 
 		self.cs_duplicate_curve_qpushbutton.clicked.connect(self.cs_duplicate_button_func)
-		# self.cs_locate_create_curve_json_qpushbutton.clicked.connect()
 		self.cs_filename()
 		self.cs_filepath()
 		self.cs_preserve_curve_qpushbutton.setEnabled(False)
 		self.cs_locate_create_curve_json_qpushbutton.clicked.connect(self.cs_locate_create_button)
+		self.cs_preserve_curve_qpushbutton.clicked.connect(self.query_curve)
 
-	# CAVEAT : Duplicate curve func
-	def cs_duplicate_button_func(self):
+	# CAVEAT : This Duplicate curve func helps to duplicate the curve
+	@staticmethod
+	def cs_duplicate_button_func():
 		cmds.duplicate()
 
-	# CAVEAT : Controller storage file name
+	# CAVEAT : This Controller storage function stores the readonly file name
 	def cs_filename(self):
 		self.cs_json_file_name_qlineedit.setText("controller_storage.json")
 		self.cs_json_file_name_qlineedit.setReadOnly(True)
 
-	# CAVEAT : Controller storage directory path
+	# CAVEAT : Controller storage directory path tooltip
 	def cs_filepath(self):
 		self.cs_locate_json_path_qlineedit.setToolTip("Provide an appropriate directory path")
-		# if self.cs_locate_json_path_qlineedit(filepath):
-		# 	self.cs_preserve_curve_qpushbutton.setEnabled(True)
-		# else:
-		# 	self.cs_preserve_curve_qpushbutton.setEnabled(False)
 
-	# CAVEAT : Locate/Create button func
+	# CAVEAT : Locate/Create button func helps to create the .json file in the mentioned directory
 	def cs_locate_create_button(self):
 		file_dir = self.cs_locate_json_path_qlineedit.text()
 		file_name = self.cs_json_file_name_qlineedit.text()
 		if file_dir:
-			filepath = os.path.join(self.cs_locate_json_path_qlineedit.text(), self.cs_json_file_name_qlineedit.text())
+			filepath = os.path.join(file_dir, file_name)  # filepath of the dir
 			if os.path.isfile(filepath):
 				print "controller_storage.json File already exists"
+				self.cs_preserve_curve_qpushbutton.setEnabled(True)
 			else:
-				with open(filepath, "w") as testing:
-					pass
+				with open(filepath, "w") as _controller_storage:
+					pass  # this creates the .json file
 				print "controller_storage.json File created"
+				self.cs_preserve_curve_qpushbutton.setEnabled(True)
 		else:
 			print "Kindly provide directory path"
 
-			print filepath
+	# CAVEAT : Preserve curve button func will preserve the curve in a .json file
+	def query_curve(self):
+		curve_sel_list_shape = cmds.listRelatives()
+		if curve_sel_list_shape:
+			if cmds.nodeType(curve_sel_list_shape[0], apiType=True) == "kNurbsCurve":
+				print "It is a kNurbsCurve"
+				# return curve_sel_list_shape
+				# print curve_sel_list_shape
+				if type(curve_sel_list_shape) == list:
+					# if we select curveshape directly that gives an error, kindly fix that in the function
+					get_curve_points = cmds.getAttr('%s.cv[*]' % curve_sel_list_shape[0])
+					get_curve_degree = cmds.getAttr('%s.degree' % curve_sel_list_shape[0])
+					# get_curve_spans = cmds.getAttr( '%s.spans'%curve_sel_list_shape[0])
+					curve_info_node = cmds.createNode('curveInfo',
+													  name="%s_controller_storage" % curve_sel_list_shape[0])
+					cmds.connectAttr('%s.worldSpace' % curve_sel_list_shape[0],
+									 '%s_controller_storage.inputCurve' % curve_sel_list_shape[0])
+					get_curve_knots = cmds.getAttr('%s_controller_storage.knots[*]' % curve_sel_list_shape[0])
+					cmds.delete(curve_info_node)
+					# cmds.curve(degree=get_curve_degree, point=get_curve_points, knot=get_curve_knots)
+					# save the script in the json file "curve_dict" first need to create a dict
+					curve_dict = {
+						"{}".format(curve_sel_list_shape[0]): "cmds.curve(degree={}, point={}, knot={})".format(
+							get_curve_degree, get_curve_points,
+							get_curve_knots)}
+					with open(os.path.join(self.cs_locate_json_path_qlineedit.text(),
+										   self.cs_json_file_name_qlineedit.text()), "w") as write_file:
+						json.dump(curve_dict, write_file, indent=4)
+						write_file.close()
+				else:
+					print curve_sel_list_shape
+			else:
+				print "Kindly pick a 'kNurbsCurve'"
+		else:
+			print "Kindly select something in the scene"
 
 # to preserve the shape of the curve
 # def query_curve():
