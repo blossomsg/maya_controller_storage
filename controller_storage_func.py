32b5dadf3dbea3405a1d581cec53a8b7a31ffe05
Blossom Ghuntla <bghuntla123@gmail.com>
2020-04-28 18:06:12 +0530

    Commit_7: controller storage preserve button func completed and removed the if and else statement to confirm if it is a list, duplicate curve function completed, qtooltip function completed, read_json for listwidget completed, updated controller_storage_ui file by removing the unwanted lines

--- controller_storage_func.py
+++ controller_storage_func.py
@@ -13,26 +13,49 @@ class ControllerStorageFunc(controller_storage_ui.ControllerStorageUI):
 	def __init__(self):
 		super(ControllerStorageFunc, self).__init__()
 
+		self.cs_tooltips()
 		self.cs_duplicate_curve_qpushbutton.clicked.connect(self.cs_duplicate_button_func)
 		self.cs_filename()
-		self.cs_filepath()
 		self.cs_preserve_curve_qpushbutton.setEnabled(False)
+		self.cs_read_json_file_qpushbutton.setEnabled(False)
 		self.cs_locate_create_curve_json_qpushbutton.clicked.connect(self.cs_locate_create_button)
-		self.cs_preserve_curve_qpushbutton.clicked.connect(self.query_curve)
+		self.cs_preserve_curve_qpushbutton.clicked.connect(self.cs_query_curve)
+		self.cs_read_json_file_qpushbutton.clicked.connect(self.cs_read_json)
+		self.cs_create_selected_curve_qpushbutton.clicked.connect(self.cs_create_curve)
+
+	# CAVEAT : Controller storage directory path tooltip
+	def cs_tooltips(self):
+		self.cs_duplicate_curve_qpushbutton.setToolTip("Creates duplicate of the curve on the spot")
+		self.cs_locate_json_path_qlineedit.setToolTip("Provide an appropriate directory path")
+		self.cs_preserve_curve_qpushbutton.setToolTip(
+			"Enables once the jason file location is confirmed,"
+			"Preserves nurbs curve in a json file(kindly provide a unique name, or it will override existing key)")
+		self.cs_locate_create_curve_json_qpushbutton.setToolTip("Confirms if the json file exists in the "
+																"mentioned path or not and enables the Preserve "
+																"Curve and Read json button")
+		self.cs_read_json_file_qpushbutton.setToolTip("Enables once the jason file location is confirmed,"
+													  "fetches the keys from the json file")
+		self.cs_curve_dict_listwid.setToolTip("Will list the names of the curves that were saved in the json file")
+		self.cs_create_selected_curve_qpushbutton.setToolTip("Creates the curve acc. to the selection from the list")
 
 	# CAVEAT : This Duplicate curve func helps to duplicate the curve
 	@staticmethod
 	def cs_duplicate_button_func():
-		cmds.duplicate()
+		curve_sel_list_shape = cmds.listRelatives()
+		print curve_sel_list_shape
+		if curve_sel_list_shape:
+			if cmds.nodeType(curve_sel_list_shape[0], apiType=True) == "kNurbsCurve":
+				print "It is a kNurbsCurve"
+				cmds.duplicate()
+			else:
+				print "Kindly pick a 'kNurbsCurve'"
+		else:
+			print "Kindly select something in the scene"
 
 	# CAVEAT : This Controller storage function stores the readonly file name
 	def cs_filename(self):
 		self.cs_json_file_name_qlineedit.setText("controller_storage.json")
 		self.cs_json_file_name_qlineedit.setReadOnly(True)
-
-	# CAVEAT : Controller storage directory path tooltip
-	def cs_filepath(self):
-		self.cs_locate_json_path_qlineedit.setToolTip("Provide an appropriate directory path")
 
 	# CAVEAT : Locate/Create button func helps to create the .json file in the mentioned directory
 	def cs_locate_create_button(self):
@@ -43,16 +66,18 @@ class ControllerStorageFunc(controller_storage_ui.ControllerStorageUI):
 			if os.path.isfile(filepath):
 				print "controller_storage.json File already exists"
 				self.cs_preserve_curve_qpushbutton.setEnabled(True)
+				self.cs_read_json_file_qpushbutton.setEnabled(True)
 			else:
 				with open(filepath, "w") as _controller_storage:
 					pass  # this creates the .json file
 				print "controller_storage.json File created"
 				self.cs_preserve_curve_qpushbutton.setEnabled(True)
+				self.cs_read_json_file_qpushbutton.setEnabled(True)
 		else:
 			print "Kindly provide directory path"
 
 	# CAVEAT : Preserve curve button func will preserve the curve in a .json file
-	def query_curve(self):
+	def cs_query_curve(self):
 		file_dir = self.cs_locate_json_path_qlineedit.text()
 		file_name = self.cs_json_file_name_qlineedit.text()
 		curve_sel_list_shape = cmds.listRelatives()  # Result: [u'curveShape2'] #
@@ -61,46 +86,55 @@ class ControllerStorageFunc(controller_storage_ui.ControllerStorageUI):
 				print "It is a kNurbsCurve"
 				# return curve_sel_list_shape
 				# print curve_sel_list_shape
-				if type(curve_sel_list_shape) == list:  # list so that the we can extract single value from it
-					get_curve_points = cmds.getAttr('%s.cv[*]' % curve_sel_list_shape[0])
-					get_curve_degree = cmds.getAttr('%s.degree' % curve_sel_list_shape[0])
-					# get_curve_spans = cmds.getAttr( '%s.spans'%curve_sel_list_shape[0])
-					curve_info_node = cmds.createNode('curveInfo',
-													  name="%s_controller_storage" % curve_sel_list_shape[0])
-					cmds.connectAttr('%s.worldSpace' % curve_sel_list_shape[0],
-									 '%s_controller_storage.inputCurve' % curve_sel_list_shape[0])
-					get_curve_knots = cmds.getAttr('%s_controller_storage.knots[*]' % curve_sel_list_shape[0])
-					cmds.delete(curve_info_node)
-					# cmds.curve(degree=get_curve_degree, point=get_curve_points, knot=get_curve_knots)
-					# save the script in the json file "curve_dict" first need to create a dict
-					if os.stat(os.path.join(file_dir, file_name)).st_size == 0:
-						print "no keys at all"
-						curve_dict = {
-							"{}".format(curve_sel_list_shape[0]): "cmds.curve(degree={}, point={}, knot={})".format(
-								get_curve_degree, get_curve_points,
-								get_curve_knots)}  # to save a single key in dict, outside of if did not make any sense
-						with open(os.path.join(file_dir, file_name), "w") as write_file:
-							json.dump(curve_dict, write_file, indent=4)
-							write_file.close()
-					else:
-						print "there are some keys in the json"
-						# reading keys to get existing values in the variable
-						with open(os.path.join(file_dir, file_name), "r") as read_file:
-							curve_dict = json.load(read_file)
-						# dict to dump with existing keys
-						curve_dict["{}".format(curve_sel_list_shape[0])] = "cmds.curve(degree={}, point={}, knot={})".format(get_curve_degree,
-																													get_curve_points,
-																													get_curve_knots)
-						# write the values in the json with new and the old ones
-						with open(os.path.join(file_dir, file_name), "w") as write_file:
-							json.dump(curve_dict, write_file, indent=4)
-							write_file.close()
+				get_curve_points = cmds.getAttr('%s.cv[*]' % curve_sel_list_shape[0])
+				get_curve_degree = cmds.getAttr('%s.degree' % curve_sel_list_shape[0])
+				# get_curve_spans = cmds.getAttr( '%s.spans'%curve_sel_list_shape[0])
+				curve_info_node = cmds.createNode('curveInfo',
+												  name="%s_controller_storage" % curve_sel_list_shape[0])
+				cmds.connectAttr('%s.worldSpace' % curve_sel_list_shape[0],
+								 '%s_controller_storage.inputCurve' % curve_sel_list_shape[0])
+				get_curve_knots = cmds.getAttr('%s_controller_storage.knots[*]' % curve_sel_list_shape[0])
+				cmds.delete(curve_info_node)
+				# cmds.curve(degree=get_curve_degree, point=get_curve_points, knot=get_curve_knots)
+				# save the script in the json file "curve_dict" first need to create a dict
+				if os.stat(os.path.join(file_dir, file_name)).st_size == 0:
+					print "Empty json file, adding first key"
+					curve_dict = {
+						"{}".format(curve_sel_list_shape[0]): "cmds.curve(degree={}, point={}, knot={})".format(
+							get_curve_degree, get_curve_points,
+							get_curve_knots)}  # to save a single key in dict, outside of if did not make any sense
+					with open(os.path.join(file_dir, file_name), "w") as write_file:
+						json.dump(curve_dict, write_file, indent=4)
+						write_file.close()
 				else:
-					print curve_sel_list_shape
+					print "there are some keys in the json"
+					# reading keys to get existing values in the variable
+					with open(os.path.join(file_dir, file_name), "r") as read_file:
+						curve_dict = json.load(read_file)
+					# dict to dump with existing keys
+					curve_dict[
+						"{}".format(curve_sel_list_shape[0])] = "cmds.curve(degree={}, point={}, knot={})".format(
+						get_curve_degree,
+						get_curve_points,
+						get_curve_knots)
+					# write the values in the json with new and the old ones
+					with open(os.path.join(file_dir, file_name), "w") as write_file:
+						json.dump(curve_dict, write_file, indent=4)
+						write_file.close()
 			else:
 				print "Kindly pick a 'kNurbsCurve'"
 		else:
 			print "Kindly select something in the scene"
+
+	def cs_read_json(self):
+		file_dir = self.cs_locate_json_path_qlineedit.text()
+		file_name = self.cs_json_file_name_qlineedit.text()
+		with open(os.path.join(file_dir, file_name), "r") as read_file:
+			curve_dict = json.load(read_file)
+		self.cs_curve_dict_listwid.addItems(curve_dict.keys())
+
+	def cs_create_curve(self):
+		print self.cs_curve_dict_listwid.selectedItems()
 
 # to preserve the shape of the curve
 # def query_curve():
--- controller_storage_ui.py
+++ controller_storage_ui.py
@@ -31,17 +31,12 @@ class ControllerStorageUI(QtWidgets.QWidget):
 		self.cs_json_file_name_qlineedit = QtWidgets.QLineEdit()
 		self.cs_preserve_curve_qpushbutton = QtWidgets.QPushButton("Preserve_Curve")
 		self.cs_read_json_file_qpushbutton = QtWidgets.QPushButton("Read_JSON_File")
-		# self.cs_json_create_checkbox = QtWidgets.QCheckBox("Create_JSON")
-		# self.cs_create_curve_json_qpushbutton = QtWidgets.QPushButton("Create_CurveJSON")
-		# self.cs_create_newjson_path_qlineedit = QtWidgets.QLineEdit()
 		self.cs_curve_dict_listwid = QtWidgets.QListWidget()
 		self.cs_create_selected_curve_qpushbutton = QtWidgets.QPushButton("Create_Selected_Curve")
 
 		# CAVEAT : Layouts
 		self.cs_vlayout = QtWidgets.QVBoxLayout()  # vertical layout
 		self.cs_locate_json_hlayout = QtWidgets.QHBoxLayout()  # horizontal layout to locate json
-		# self.cs_create_json_hlayout = QtWidgets.QHBoxLayout()  # horizontal layout to create json
-		# self.cs_create_json_set_vlayout = QtWidgets.QVBoxLayout() # vertically organize "create json"
 
 		# CAVEAT : Adding widgets to layouts
 		self.cs_vlayout.addWidget(self.cs_duplicate_curve_qpushbutton)
@@ -52,11 +47,6 @@ class ControllerStorageUI(QtWidgets.QWidget):
 		self.cs_vlayout.addLayout(self.cs_locate_json_hlayout)
 		self.cs_vlayout.addWidget(self.cs_preserve_curve_qpushbutton)
 		self.cs_vlayout.addWidget(self.cs_read_json_file_qpushbutton)
-		# self.cs_create_json_hlayout.addWidget(self.cs_create_curve_json_qpushbutton)
-		# self.cs_create_json_hlayout.addWidget(self.cs_create_newjson_path_qlineedit)
-		# self.cs_create_json_set_vlayout.addWidget(self.cs_json_create_checkbox)
-		# self.cs_create_json_set_vlayout.addLayout(self.cs_create_json_hlayout)
-		# self.cs_vlayout.addLayout(self.cs_create_json_set_vlayout)
 		self.cs_vlayout.addWidget(self.cs_curve_dict_listwid)
 		self.cs_vlayout.addWidget(self.cs_create_selected_curve_qpushbutton)
 
